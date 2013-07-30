63037359c5a767a1e6d0779a5971f15db37a1c26
MakerSquare Student <students@themakersquare.com>
2013-07-30 18:58:20 -0500

    Added Devise and Soundcloud authorization using omniauth gem. I've been following the rails cast #235. I'll follow up with #236 afterwards

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onemusic::Application.config.secret_token = '224284d32c678d403aad0846bc23066f5e7684a15579a8e175bcb45ff9d8190476ad7c55903bffa44cc0c2484e95aba6cdc85b99b69c287d92bdfb139ea886fd'
